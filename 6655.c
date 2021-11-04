d81585bc142b036102aeaa7b33a1c0ac30e5b646
Fatema Moharam <5445035-moharamfatema@users.noreply.gitlab.com>
2021-11-05 00:16:09 +0200

    improvements on SIGCHLD handler

--- 6655.c
+++ 6655.c
@@ -14,12 +14,9 @@
 #define MAXARGS 32
 
 FILE * fptr ;
-pid_t childpid;
 
 void handler(int sig){
-	if(childpid >0)
-		fprintf(fptr,"Process %d terminated\n",childpid);
-	
+	fprintf(fptr,"Child process terminated\n");
 }
 
 int main(){
@@ -37,17 +34,16 @@ int main(){
 	char* args[MAXARGS];
 	int noofargs = 0;
 	int status;
-
+	int childid;
 
 	/*extracting command from user*/
 	while(1){
 		setbuf(stdout,NULL);
 		printf("SHELL>> ");
 		scanf("%[^\n]s",command);
-		printf("command : %s\n",command);
 		setbuf(stdin,NULL);
 		if((strcmp(command,"exit")==0) || (strcmp(command,"exit &")==0))
-			return 0;
+			exit(0);
 		
 		rest = command;
 		noofargs = 0;
@@ -61,58 +57,47 @@ int main(){
 					
 		/*wait flag*/
 		int8_t willWait=(strcmp(args[noofargs-1],"&")==0)?0:1;
-		printf("will wait? %d\n",willWait);
+		//printf("will wait? %d\n",willWait);
 		if (!willWait){
 			args[noofargs-1] = NULL;
+		}else{
+			//printf("----------WAITING FOR CHILD------------\n");
 		}
-		/*new clean arr of args*/
-		/*char newargs[noofargs+1][ARGLEN];
-		int k = 0;
-		for(k = 0;k<((willWait)?noofargs-1:noofargs-2);k++){
-			strcpy(newargs[k],args[k+1]);	
-			strcat(newargs[k],"\0");	
-			//printf("%d\t%s\n",k,newargs[k]);
-			if(k>MAXARGS)
-				break;
-		}//*newargs[k++]=NULL;*/
 		
-		/*printing args[]*/
+		/*printing args[]*//*
 		printf("Arguments are:\n");
 		for (int i = 0; i<noofargs;i++){
 			if(args[i])
 				printf("%d\t%s\n",i,args[i]);
 			else
 				break;
-		}
+		}*/
 		
-		
-		//TODO:working except execvp			
 		/*executing command*/
-		int childid = fork();
+		childid = fork();
 
-		printf("childid = %d\n",childid);
+		//printf("childid = %d\n",childid);
 		if(childid == 0){
 			/*child code*/
+			//printf("----------------CHILD PROCESS--------------\n");
+			int8_t flag = execvp(args[0],&args[0]);
+			//INCASE OF ERROR
+			if (flag)
+			{
+				perror("Execution unsuccessful ");
+				return 0;
+			}
 			
-			printf("I am a child\n");
-
-			//char* tmp[] = {"ls",NULL};
-			execvp(args[0],&args[0]);
-
-			return 0;
 		}else if(childid < 0){
-			perror("fork unsuccessfull\n");
+			perror("Fork unsuccessfull");
 		}else{
 			/*parent code*/
-			childpid = childid;
-
-			printf("I am the parent\n");
 			if(willWait){
 				waitpid(childid,&status,0);
 			}
 
 		}
-		printf(childid>0?"I am the parent and am still alive!!\n":"I should not print\n");		
+		//printf(childid>0?((willWait)?"-----------------BACK TO PARENT----------------\n":"\n"):"\n");		
 	}
 	fclose(fptr);
 	return 0;
